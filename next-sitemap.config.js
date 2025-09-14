7f3ce728c825930616609fc558c7088c8cd455de
Muradhossain19 <muradhossain.md1997@gmail.com>
2025-09-15 02:02:32 +0600

    ..

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-  siteUrl: "https://www.muradhossain.com", // আপনার মূল domain দিন
+  siteUrl: "https://www.muradhossain.com",
   generateRobotsTxt: true,
   changefreq: "weekly",
   priority: 0.7,
+  exclude: ["/dashboard", "/dashboard/*", "/login", "/admin", "/admin/*"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: [
+          "/dashboard",
+          "/dashboard/",
+          "/dashboard/*",
+          "/login",
+          "/admin",
+          "/admin/",
+          "/admin/*",
+        ],
+        allow: "/",
+      },
+    ],
+    host: "https://www.muradhossain.com",
+    sitemap: "https://www.muradhossain.com/sitemap.xml",
+  },
 };
